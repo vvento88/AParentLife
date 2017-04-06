9ae28a8576c45ed4d45f9be5d001e5d256e0acdc
Victor <victor.vento@gmail.com>
2017-04-06 14:15:37 -0600

    setting up files and folders and automation

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  watch(['./app/*.html', './app/*.php'], function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/style/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
+  watch('./app/assets/scripts/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  });
+});
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/style/style.css')
+    .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+  browserSync.reload();
+});
---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+require('./gulp/tasks/style');
+require('./gulp/tasks/watch');
+require('./gulp/tasks/sprites');
+require('./gulp/tasks/scripts');
+require('./gulp/tasks/modernizr');
+require('./gulp/tasks/build');
